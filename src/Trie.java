220be60dad0f4736e3f65ddd6c8ba0b986600262
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-01-23 22:28:00 +0330

    Trie and TrieNode added

---
+++ src/Trie.java
@@ -0,0 +1,56 @@
+public class Trie {
+    private final TrieNode root;
+    public Trie(){
+        this.root = new TrieNode();
+    }
+
+    public void insertBank(String word , Bank bank){
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = getIndex(i , word);
+            if (current.getChildren()[index] == null){
+                TrieNode trieNode = new TrieNode();
+                current.setChildren(index , trieNode);
+            }
+            current = current.getChildren()[index];
+        }
+        current.setWord(true);
+        current.setBank(true);
+        current.setBank(bank);
+    }
+
+    public void insertNeighborhood(String word , Neighborhood neighborhood){
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = getIndex(i , word);
+            if (current.getChildren()[index] == null){
+                TrieNode trieNode = new TrieNode();
+                current.setChildren(index , trieNode);
+            }
+            current = current.getChildren()[index];
+        }
+        current.setWord(true);
+        current.setBank(false);
+        current.setNeighborhood(neighborhood);
+    }
+
+    public TrieNode search(String word){
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = getIndex(i , word);
+            if (current.getChildren()[index] == null)
+                return null;
+            current = current.getChildren()[index];
+        }
+        return current;
+    }
+
+    public int getIndex(int c , String word){
+        int index = -1;
+        if (word.charAt(c) > 64 && word.charAt(c) < 91)
+            index = word.charAt(c) + 26 - 'A';
+        else if (word.charAt(c) > 96 && word.charAt(c) < 122)
+            index = word.charAt(c) - 'a';
+        return index;
+    }
+}
---
+++ src/TrieNode.java
@@ -0,0 +1,55 @@
+public class TrieNode {
+    private TrieNode[] children;
+    private boolean isWord;
+    private Bank bank;
+    private Neighborhood neighborhood;
+    private boolean isBank;
+
+    public TrieNode() {
+        this.children = new TrieNode[52];
+        this.isWord = false;
+        for (int i = 0; i < 52; i++) {
+            children[i] = null;
+        }
+    }
+
+    public TrieNode[] getChildren(){
+        return children;
+    }
+
+    public void setChildren(int i , TrieNode trieNode){
+        this.children[i] = trieNode;
+    }
+
+    public boolean isWord() {
+        return isWord;
+    }
+
+    public void setWord(boolean word) {
+        isWord = word;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    public Neighborhood getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(Neighborhood neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public boolean isBank() {
+        return isBank;
+    }
+
+    public void setBank(boolean bank) {
+        isBank = bank;
+    }
+}
